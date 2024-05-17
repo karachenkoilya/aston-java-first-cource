00dc18b9dcd684ac2cf4c3515d49fdcf5bfb497a
Ilya Karachenko <mail@karachenko.ru>
2024-05-17 15:36:13 +0500

    Задание 02-03 выполнено

---
+++ src/ru/karachenkoilya/tasks/task02/task02_03/Computer.java
@@ -0,0 +1,22 @@
+package ru.karachenkoilya.tasks.task02.task02_03;
+
+public class Computer {
+
+    public static class Processor {
+        private String model = "Intel";
+
+        public String getDetails() {
+            return model;
+        }
+    }
+
+    public class RAM {
+        private String size = "16 Gb";
+
+        public String getDetails1() {
+            return size;
+        }
+    }
+}
+
+
