703c89ed6ebbcd4ee40a21a0dba26fae304f479b
Ilya Karachenko <mail@karachenko.ru>
2024-05-24 17:18:33 +0500

    Задание 03-03 выполнено, но еще не понятно почему не обрабатывается null

--- src/ru/karachenkoilya/tasks/task03/task03_03/Solution.java
+++ src/ru/karachenkoilya/tasks/task03/task03_03/Solution.java
@@ -8,7 +8,7 @@ import static ru.karachenkoilya.tasks.task03.task03_01.StringReader.readStrings;
 
 public class Solution {
     public static void main(String[] args) {
-        var list = readStrings(1);
+        var list = readStrings(3);
 
         list.stream()
                 .flatMap(s -> Arrays.stream(s.split(" ")))
